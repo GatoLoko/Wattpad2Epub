d013fd43fdb667767748129ed96f3fe0a01feb00
Joeri-G <3677joeri@gmail.com>
2021-11-27 20:09:44 +0100

    Added non-blocking status messages, license and fixed attatched get_book

--- libs/gui.py
+++ libs/gui.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2019 Joeri-G
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 """
 Created on 27-11-2021
 
@@ -8,21 +26,28 @@ Basic GUI implementation with easygui
 https://easygui.readthedocs.io/en/latest/
 ===
 """
+import threading, sys
 try:
     import easygui
 except ImportError:
     raise
 
+VERSION = '1.0'
+
 class GUI(object):
-    
+
     STANDARD_TITLE = "Wattpad Downloader [v%s]" % VERSION
     STANDARD_LOCATION = "%USERPROFILE%\\Documents" if sys.platform == "win32" else "~/Documents"
 
     def __init__(self):
         super(GUI, self).__init__()
-        self.initial_GUI()
+        self.download_function = lambda url, location: print("Download function has not been attatched")
 
-    def initial_GUI(self):
+
+    def initialize_GUI(self):
+        self.mode_selection_screen()
+
+    def mode_selection_screen(self):
         # check how many eboos the user wants to download
         choice_ist = ('Single Story Download', 'Bulk Download')
         choice = easygui.choicebox(msg="Welcome to Wattpad Downloader",
@@ -56,7 +81,7 @@ class GUI(object):
             return
         try:
             if easygui.ynbox(msg="Download [%s]?" % url, title=self.STANDARD_TITLE):
-                get_book(url, savelocation)
+                self.download_function(url, savelocation)
                 easygui.msgbox(msg="Download Finnished.\nFile saved to [%s]" % savelocation,
                                title=self.STANDARD_TITLE)
             else:
@@ -80,14 +105,21 @@ class GUI(object):
             # skip if line is empty
             if not url or url.isspace():
                 continue
-            easygui.msgbox(msg="Downloading [%s]" % url, title=self.STANDARD_TITLE)
-            # TODO: make this msg run on another thread so that it does not halt the downloads
+            self.create_threaded_msgbox("Downloading [%s]" % url)
             try:
                 print(url)
-                get_book(url, savelocation)
+                self.download_function(url, savelocation)
             except Exception as e:
                 errmsg = "It looks like something went wrong... Your download failed.\n\n%s" % str(e)
                 easygui.msgbox(msg=errmsg, title=self.STANDARD_TITLE)
 
         easygui.msgbox(msg="Downloads Finnished.\nFiles saved to [%s]" % savelocation,
                        title=self.STANDARD_TITLE)
+
+    # Create a msgbox on a separate thread so that it will not halt the process
+    # Useful when downloading several ebooks in succession
+    def create_threaded_msgbox(self, msg):
+        # create lambda function to make the creation of the thread less of an issue
+        shorthand = lambda text: easygui.msgbox(msg=text, title=self.STANDARD_TITLE)
+        thr = threading.Thread(target=shorthand, args=(msg,), daemon=True)
+        thr.start()
--- wattpad2epub.py
+++ wattpad2epub.py
@@ -215,5 +215,7 @@ if __name__ == "__main__":
 
     if len(args.initial_url) == 0 and not args.cli:
          gui = GUI() # start GUI if no URL has been specified
+         gui.download_function = get_book
+         gui.initialize_GUI()
     elif len(args.initial_url) == 1 or args.cli:
          get_book(args.initial_url[0], mypath)
